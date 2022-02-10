6940bceb800f16d03ae98e02c7dc1cead5441811
Volodymyr <volodymyr.steblii@gmail.com>
2022-02-10 14:24:32 +0200

    Update static folders config

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,23 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+    res.send('Got a GET request')
 });
+
+router.post('/', function (req, res) {
+    res.send('Got a POST request')
+})
+
+router.put('/', function (req, res) {
+    res.send('Got a PUT request')
+})
+
+router.patch('/', function (req, res) {
+    res.send('Got a PATCH request')
+})
+
+router.delete('/', function (req, res) {
+    res.send('Got a DELETE request')
+})
 
 module.exports = router;
