3f76db1af076b4d968abaf9521f91aced9f76f05
Volodymyr <volodymyr.steblii@gmail.com>
2022-02-11 09:07:44 +0200

    Remove import

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-// const userRouter = require("./user_router");
 
 // ====================== Routers Setup
 const throwRouter = require("./routes/throw");
